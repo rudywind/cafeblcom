a9ecc96ae2141d49aeb52ddff1ea0b5eebf67d5b
rudywind <35211883+rudywind@users.noreply.github.com>
2023-07-13 21:20:51 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -108,3 +108,68 @@ function autoClickAff() {
   affiliates.click();
 }
 setTimeout(autoClickAff, 10000);
+
+function launch() {
+var urlaffiliates = {
+  "linkaff": [
+    "https://tokopedia.link/caz8v3Tc0Ab",
+"https://tokopedia.link/6WiaXmTc0Ab",
+"https://tokopedia.link/ON96jJSc0Ab",
+"https://tokopedia.link/xVATPyRc0Ab",
+"https://tokopedia.link/D1lajUNc0Ab",
+    "https://s.lazada.co.id/s.PwIkF?cc",
+    "https://s.lazada.co.id/s.PwIkd?cc",
+    "https://s.lazada.co.id/s.PwIQs?cc",
+    "https://shope.ee/6zpjwWlI2r",
+    "https://shope.ee/8UeXjE3GM7",
+    "https://shope.ee/AK6Bua9gEz",
+    "https://shope.ee/7f5QjftVDp",
+    "https://shope.ee/3VFrlyNqQ0",
+    "https://shope.ee/9UX4uvKU4b",
+    "https://shope.ee/9eqV7DFqzr",
+    "https://shope.ee/4V8OxiDapp",
+    "https://shope.ee/LIq03GOSe",
+    "https://shope.ee/7ziH88R8jK",
+    "https://shope.ee/9KDeia4KN0",
+    "https://shope.ee/3Ad1NF1PBg",
+    "https://shope.ee/7pOqvnOFdv",
+    "https://shope.ee/6pWJjxASXJ",
+    "https://shope.ee/8UeXj0i2zS",
+    "https://shope.ee/3psiAQZLUg",
+    "https://shope.ee/6Ka3913lFT",
+    "https://shope.ee/3fZHy6s3Ga",
+    "https://shope.ee/3VFrlnSDeJ",
+    "https://shope.ee/6Ka38zvVFU",
+    "https://shope.ee/1ArwzUohm4",
+    "https://shope.ee/1VUnO6RZi6",
+    "https://shope.ee/7f5QjQmV4X",
+    "https://shope.ee/6zpjwCXRI1",
+    "https://shope.ee/fzbKgHZs",
+    "https://shope.ee/9UX4umbSLZ",
+    "https://shope.ee/8pHO7YMKJ7",
+    "https://shope.ee/1LBNBlFkWn",
+    "https://shope.ee/6Ka38wWJgl",
+    "https://shope.ee/3Ad1N7Qlz3",
+    "https://shope.ee/10YWn8HQFU",
+    "https://shope.ee/6pWJjqZHln",
+    "https://shope.ee/6UtTLEIxJB",
+    "https://shope.ee/1VUnO2KBXO",
+    "https://shope.ee/3Ad1N5ruVM",
+    "https://shope.ee/9p9vJLB06u",
+    "https://shope.ee/8zaoJns6cr",
+    "https://shope.ee/5AO5kkkdEJ",
+    "https://shope.ee/6fCtXVNOJk",
+    "https://shope.ee/A9mlhvsOp7",
+    "https://shope.ee/2fgkm8z5eb",
+    "https://shope.ee/1ArwzNijWo",
+    "https://shope.ee/6Ka38ryjbf",
+    "https://shope.ee/4plFM6iJDP",
+    "https://shope.ee/7KSaKeiwMa"
+  ]
+};
+  var linkaff = urlaffiliates.linkaff;
+  var randomIndex = Math.floor(Math.random() * linkaff.length);
+  var randomUrl = linkaff[randomIndex];
+
+  window.open(randomUrl);
+}
